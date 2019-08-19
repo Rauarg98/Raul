b4a3e7e801f1d9123f24b8451686ccf26d288e7f
jorge.diaz <jorge.diaz@CDS1UNIVO28>
2019-08-19 10:57:27 -0600

    k

---
+++ Xamarin2/Xamarin2/Model/DatosEsModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin2.Model
+{
+    public class DatosEsModel : ClaseBase
+    {
+        private int _Id2;
+
+        public int Id2
+        {
+            get { return _Id2; }
+            set { _Id2 = value; }
+        }
+
+        private string _NombreEs2;
+
+        public string NombreEs2
+        {
+            get { return _NombreEs2; }
+            set { _NombreEs2 = value; }
+        }
+
+        private string _ApellidoEs2;
+
+        public string ApellidoEs2
+        {
+            get { return _ApellidoEs2; }
+            set { _ApellidoEs2 = value; }
+        }
+
+        private string _Curso2;
+
+        public string  Curso2
+        {
+            get { return _Curso2; }
+            set { _Curso2 = value; }
+        }
+
+        private int _EdadEs2;
+
+        public int EdadEs2
+        {
+            get { return _EdadEs2; }
+            set { _EdadEs2 = value; }
+        }
+
+        private string _DireccionEs2;
+
+        public string DireccionEs2
+        {
+            get { return _DireccionEs2; }
+            set { _DireccionEs2 = value; }
+        }
+
+        private string _Emergencia2;
+
+        public string Emergerncia2
+        {
+            get { return _Emergencia2; }
+            set { _Emergencia2 = value; }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Xamarin2/Xamarin2/Model/DatosModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Xamarin2.Model
+{
+   public class DatosModel
+    {
+
+        public int Id;
+        public string NombreEs;
+        public string ApellidoEs;
+        public string Curso;
+        public int EdadEs;
+        public string DireccionEs;
+        public string Emergencias; 
+
+    }
+}
